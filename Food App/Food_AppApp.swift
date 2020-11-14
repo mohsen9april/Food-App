70d66718a8b48164e4700e831dff66ea9ae98b0d
Mohsen Abdollahi <mohsen9april@gmail.com>
2020-11-14 19:00:53 +0330

    Initial Commit

---
+++ Food App/Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AppApp.swift
+//  Food App
+//
+//  Created by Mohsen Abdollahi on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
